42c68d4fa154f2e1d8a7290e80d195f6891fbc63
vengaladevisriprasad <vengaladevisriprasad@gmail.com>
2026-03-26 11:11:47 +0530

    Update q3.java

--- q3.java
+++ q3.java
@@ -1,17 +1,1 @@
-import java.util.Scanner;
 
-class q3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter 3 numbers: ");
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int n3 = sc.nextInt();
-
-        System.out.println("Is the first number the largest? " + (n1 > n2 && n1 > n3));
-        System.out.println("Is the second number the largest? " + (n2 > n1 && n2 > n3));
-        System.out.println("Is the third number the largest? " + (n3 > n1 && n3 > n2));
-        sc.close();
-    }
-}
